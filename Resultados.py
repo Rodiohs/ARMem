832ba50676b178c0540e2abc827c2a58dad6bce3
JairoGH16 <162930229+JairoGH16@users.noreply.github.com>
2024-03-15 13:13:20 -0600

    Resultados de niveles y final
    Se subió el archivo que contiene las funciones para mostrar los resultados por nivel y los resultados finales del juego.

---
+++ Resultados.py
@@ -0,0 +1,34 @@
+import time
+
+def resultados_nivel(lista_jugadores:list,n):
+    print("Nivel",n,"Resultados de la ronda")
+    time.sleep(2)
+    ordenar_lista_jugadores(lista_jugadores,n)
+    for jugador in lista_jugadores:
+        print("Tiempo de",jugador[0],":",jugador[n])
+        time.sleep(1.5)
+        
+def resultados_final(lista_jugadores:list):
+    print("Resultados finales del juego...")
+    for jugador in lista_jugadores:
+            jugador.append(jugador[1]+jugador[2]+jugador[3])
+    time.sleep(2)
+    n=4
+    ordenar_lista_jugadores(lista_jugadores,n)
+    for jugador in lista_jugadores:
+        time.sleep(1.5)
+        print(jugador[0],":",jugador[4])
+    
+def ordenar_lista_jugadores(lista_jugadores:list,n):
+    contador=0
+    while contador<(len(lista_jugadores))-1:
+        if lista_jugadores[contador][n]<lista_jugadores[contador+1][n]:
+            temporal=lista_jugadores[contador]
+            lista_jugadores[contador]=lista_jugadores[contador+1]
+            lista_jugadores[contador+1]=temporal
+        contador+=1
+    contador=0
+    while contador<(len(lista_jugadores))-1:
+        if lista_jugadores[contador][n]<lista_jugadores[contador+1][n]:
+            ordenar_lista_jugadores(lista_jugadores,n)
+        contador+=1
