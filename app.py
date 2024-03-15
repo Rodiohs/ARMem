a76593546b2d93fd2706833d02144eabbc60dc21
Rodiohs <163095339+Rodiohs@users.noreply.github.com>
2024-03-15 14:48:42 -0600

    Integrar el menu en el juego
    Se integro la funcion menu a app.py para crear la lista de jugadores mediante el registro

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 import ARMem as ar
+import menu
+import Resultados
 import time
 import random
+lista_jugadores=[]
 lista_juego=[0,1,2,3,4]
-
+menu.menu_inicio(lista_jugadores)
 print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 tiempo_total=0
 for n in range(1,4):
-    for j in lista_jugadores:
-        lista_juego_actual = random.sample(lista_juego,2+n)
-        print(f'Memorice la siguiente secuencia...')
-        print(lista_juego_actual)
-        time.sleep(1)
-        print(f'Ordene los marcadores en el orden que se le indicó!')
-        time.sleep(1)
-        
-        print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+    for r in range(0,5):
+        for j in lista_jugadores:
+            lista_juego_actual = random.sample(lista_juego,2+n)
+            print(f'Memorice la siguiente secuencia...')
+            print(lista_juego_actual)
+            time.sleep(1)
+            print(f'Ordene los marcadores en el orden que se le indicó!')
+            time.sleep(1)
+            
+            print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 
-        tiempo_partida=round(ar.start_sorting(lista_juego_actual,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
-        tiempo_total+=tiempo_partida
-        print(f'Tiempo de partida: {tiempo_partida}s')
+            tiempo_partida=round(ar.start_sorting(lista_juego_actual,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
+            tiempo_total+=tiempo_partida
+            print(f'Tiempo de partida: {tiempo_partida}s')
 
-        time.sleep(1)
-        print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+            time.sleep(1)
+            print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+    Resultados.resultados_nivel(lista_jugadores,n)
+Resultados.resultados_final(lista_jugadores)
