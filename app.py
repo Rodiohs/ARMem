6ec4cb9541d1c2d7ef8265250212b2218935ec03
Rodiohs <163095339+Rodiohs@users.noreply.github.com>
2024-03-15 15:37:56 -0600

    Agregar el tiempo a las sublistas cada ronda
    Se programo el codigo para que al final de cada ronda se sumara el tiempo de la partida al tiempo que el jugador dura en cada nivel y para mostrar los tiempos en los resultados.

--- app.py
+++ app.py
@@ -3,28 +3,39 @@ import menu
 import Resultados
 import time
 import random
+
+def juego():
+    menu.menu_inicio(lista_jugadores)
+
+    for jugador in lista_jugadores:
+        jugador.extend([0,0,0])
+    print(lista_jugadores)
+
+    print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+    tiempo_total=0
+    for n in range(0,3):
+        for r in range(0,2):
+            for jugador in lista_jugadores:
+                lista_juego_actual = random.sample(lista_juego,3+n)
+                print(f'Memorice la siguiente secuencia...')
+                print(lista_juego_actual)
+                time.sleep(1)
+                print(f'Ordene los marcadores en el orden que se le indicó!')
+                time.sleep(1)
+                
+                print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+
+                tiempo_partida=round(ar.start_sorting(lista_juego_actual,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
+                tiempo_total+=tiempo_partida
+                print(f'Tiempo de partida: {tiempo_partida}s')
+
+                time.sleep(1)
+                print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+                jugador[n+1]+=tiempo_partida
+
+        Resultados.resultados_nivel(lista_jugadores,n)
+    Resultados.resultados_final(lista_jugadores)
+
 lista_jugadores=[]
 lista_juego=[0,1,2,3,4]
-menu.menu_inicio(lista_jugadores)
-print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-tiempo_total=0
-for n in range(1,4):
-    for r in range(0,5):
-        for j in lista_jugadores:
-            lista_juego_actual = random.sample(lista_juego,2+n)
-            print(f'Memorice la siguiente secuencia...')
-            print(lista_juego_actual)
-            time.sleep(1)
-            print(f'Ordene los marcadores en el orden que se le indicó!')
-            time.sleep(1)
-            
-            print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-
-            tiempo_partida=round(ar.start_sorting(lista_juego_actual,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
-            tiempo_total+=tiempo_partida
-            print(f'Tiempo de partida: {tiempo_partida}s')
-
-            time.sleep(1)
-            print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-    Resultados.resultados_nivel(lista_jugadores,n)
-Resultados.resultados_final(lista_jugadores)
+juego()
