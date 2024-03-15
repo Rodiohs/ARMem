ef830294204d9b66d7a8e07a58219ca7e2d3cb74
Rodiohs <163095339+Rodiohs@users.noreply.github.com>
2024-03-15 14:08:15 -0600

    For para niveles y for para jugadores.py
    Se agregó un for que recorre los tres niveles y otro for para recorrer cada jugador.

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import ARMem as ar
 import time
-lista_juego=[[0,1,2,3,4],[4,3,2,1,0],[4,3,2,1,0],[0,2,1,4,3],[3,4,2,0,1]]
+import random
+lista_juego=[0,1,2,3,4]
 
 print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 tiempo_total=0
-for x in lista_juego:
-    print(f'Memorice la siguiente secuencia...')
-    print(x)
-    time.sleep(5)
-    print(f'Ordene los marcadores en el orden que se le indicó!')
-    time.sleep(3)
-    
-    print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
+for n in range(1,4):
+    for j in lista_jugadores:
+        lista_juego_actual = random.sample(lista_juego,2+n)
+        print(f'Memorice la siguiente secuencia...')
+        print(lista_juego_actual)
+        time.sleep(1)
+        print(f'Ordene los marcadores en el orden que se le indicó!')
+        time.sleep(1)
+        
+        print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
 
-    tiempo_partida=round(ar.start_sorting(x,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
-    tiempo_total+=tiempo_partida
-    print(f'Tiempo de partida: {tiempo_partida}s')
+        tiempo_partida=round(ar.start_sorting(lista_juego_actual,flip_image=True,show_images=True, show_coordinates=False, show_ids=False, show_identified_marker=False)  ,2)
+        tiempo_total+=tiempo_partida
+        print(f'Tiempo de partida: {tiempo_partida}s')
 
-    time.sleep(3)
-    print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
-
-print (f'El usuario ha completado el juevo en un tiempo de {tiempo_total}s')
+        time.sleep(1)
+        print('\033[2J')  # Código ANSI para limpiar la pantalla en sistemas Windows
