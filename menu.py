ccd18dbf6a4f4683974a877769268b0297c80ca1
JairoGH16 <162930229+JairoGH16@users.noreply.github.com>
2024-03-16 23:26:26 -0600

    Update menu.py
    Las funciones menu_inicio y registro se mezclaron en una sola para acortar el código, eliminando lineas innecesarias y para tener un menú más fácil de entender

--- menu.py
+++ menu.py
@@ -1,55 +1,61 @@
 import time
 
-numero_jugador = 0
-lista_jugadores = []
+def menu_inicio(lista_jugadores:list):
+    """menu_inicio es una función que facilita al usuario la interfaz mediante la cuál puede seleccionar las acciones a
+    realizar antes de jugar, las cuales son agregar a los jugadores, eliminar a los jugadores agregados, empezar el juego
+    y salir del programa.
 
-def menu_inicio(lista_jugadores):
-    print (chr(27) + "[2J")
-    print("¡Juego de ordenar!\nMenú principal\n1 - Registro de jugadores\n2 - Iniciar el juego")
-    opcion=(input("Escriba 1 o 2 para seleccionar una acción: "))
-    if opcion!="1" and opcion!="2":
-        print("Escriba únicamente 1 o 2 para la selección de una acción.")
-        time.sleep(3)
-        print (chr(27) + "[2J")
-        menu_inicio()
-    if opcion=="1":
-         registro(lista_jugadores,numero_jugador)
-    if opcion=="2":
-        return(lista_jugadores)
-
-def registro(lista_jugadores,numero_jugador):
+    Args:
+        lista_jugadores (list): la lista de los jugadores se recibe estando vacía y en la función menú se le agregan los
+        nombres de los jugadores
+    
+    Autor:
+        Jairo González
+    """
+    numero_jugador = 0
     while True:
         print (chr(27) + "[2J")
         for elemento in lista_jugadores:
             print(elemento[0],end=" . ")
-        print("\n¡Registro de jugadores!\n1 - Registrar un jugador nuevo\n2 - Olvidar a todos los jugadores\n3 - Salir al menú para jugar")
+        print("\n¡Menú principal!\n1 - Registrar un jugador nuevo\n2 - Olvidar a todos los jugadores\n3 - Jugar\n4 - Salir")
         opcion=(input("Escriba 1, 2 o 3 para seleccionar una acción: "))
-        if opcion!="1" and opcion!="2" and opcion!="3":
-             print("Escriba únicamente 1, 2 o 3 para la selección de una acción.")
-             time.sleep(3)
+        if opcion!="1" and opcion!="2" and opcion!="3" and opcion!="4":
+             input("Escriba únicamente 1, 2, 3 o 4 para la selección de una acción.\nOk: Enter")
+        if opcion=="4":
+            exit()
         if opcion=="3":
-             return
+             if len(lista_jugadores)>0:
+                 return
+             else:
+                 input("Debe haber almenos un jugador.\nOk: Enter")
         if opcion=="2":
              print("Jugadores olvidados completamente")
              time.sleep(2)
              lista_jugadores.clear()
              numero_jugador=0
         if opcion=="1":
-            nombre=input("Inserte un nombre o una S para salir al menú: ")
-            if nombre=="S":
-                 return
-            lista_jugadores.append([])
-            lista_jugadores[numero_jugador].append(nombre)
-            numero_jugador+=1
+            nombre=input("Inserta un nombre, o si no desea registrar otro jugador solamente presiona enter y deja el espacio vacío.\n")
+            if nombre!="":
+                lista_jugadores.append([])
+                lista_jugadores[numero_jugador].append(nombre)
+                numero_jugador+=1
 
 def remenu(lista_jugadores):
+    """remenu da un pequeño aviso de que la partida va a comenzar en los casos en los que los jugadores repitan el juego
+    con los mismos usuarios, sin tener que pasar por menu_inicio
+
+    Args:
+        lista_jugadores (_type_): la lista de los jugadores
+    Autor:
+        Rafael Odio
+    """
     time.sleep(1)
-    print("Preparense para jugar de nuevo!")
+    print("¡Preparense para jugar de nuevo!")
     time.sleep(1)
-    print("3!")
+    print("¡3!")
     time.sleep(1)
-    print("2!")
+    print("¡2!")
     time.sleep(1)
-    print("1!")
+    print("¡1!")
     time.sleep(1)
     return(lista_jugadores)
